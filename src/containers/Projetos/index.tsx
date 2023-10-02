41a8cb29c203406fd97ad88c4fa5126033a6531e
lindbergpg <Lindbergpg@gmail.com>
2023-10-02 13:23:11 -0300

    criando a secao de projetos e o componente de projeto

--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,37 @@
+import Projeto from '../../conponents/Projeto'
 import Titulo from '../../conponents/Titulo'
+
+import { Lista } from './styles'
 
 const Projetos = () => (
   <section>
     <Titulo fontSize={16}>Projetos</Titulo>
+    <Lista>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+      <li>
+        <Projeto></Projeto>
+      </li>
+    </Lista>
   </section>
 )
 
