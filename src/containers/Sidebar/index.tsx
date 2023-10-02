001659c29f25fd2df0bad5001137256f89b271c2
lindbergpg <Lindbergpg@gmail.com>
2023-10-02 19:49:31 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../conponents/Avatar'
+import Paragrafo from '../../conponents/Paragrafo'
 import Titulo from '../../conponents/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lindbergpg.png" alt="" />
-    <Titulo fontSize={20}>Lindberg Pereira</Titulo>
+    <SidebarContainer>
+      <Avatar></Avatar>
+      <Titulo fontSize={20}>Lindberg Pereira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        lindbergpg
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
